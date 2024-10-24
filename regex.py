bf6d37f532a7dcf699b1ebc3415fa869c2f7a107
David González Martínez <me@davidgonmar.me>
2024-10-24 17:51:18 +0200

    cleanup

--- regex.py
+++ regex.py
@@ -1,13 +1,16 @@
-from typing import List, Dict, Set, Hashable
+from typing import Dict, Set, Hashable
 from collections import defaultdict
 
 NFAState = Hashable
 
 EPSILON = ""
 
+
 class NFA:
-    def __init__(self, states: List[NFAState], start: NFAState):
+    def __init__(self, states: Set[NFAState], start: NFAState):
         self.states = states
+        if start not in self.states:
+            self.states.add(start)
         self.start = start
         self.accept = set()
         self.trasitions: Dict[NFAState, Dict[str, Set[NFAState]]] = defaultdict(
@@ -18,19 +21,34 @@ class NFA:
         self.trasitions[src][symbol].add(dest)
 
     def add_state(self, state: NFAState):
-        self.states.append(state)
+        self.states.add(state)
 
     def add_accept(self, state: NFAState):
         assert state in self.states, "State must be added before it can be accepted"
         self.accept.add(state)
 
+    def epsilon_closure(self, states):
+        closure = set(states)
+        stack = list(states)
+        while stack:
+            state = stack.pop()
+            for next_state in self.trasitions[state][EPSILON]:
+                if next_state not in closure:
+                    closure.add(next_state)
+                    stack.append(next_state)
+        return closure
+
     def match(self, string: str) -> bool:
         current_states = {self.start}
+        current_states = self.epsilon_closure(current_states)
+
         for symbol in string:
             next_states = set()
             for state in current_states:
                 next_states |= self.trasitions[state][symbol]
-                next_states |= self.trasitions[state][EPSILON]
+
+            next_states = self.epsilon_closure(next_states)
+
             current_states = next_states
         return bool(
             current_states & self.accept
@@ -45,11 +63,11 @@ class NFA:
             for symbol, dest_states in transitions.items():
                 new_nfa.trasitions[state][symbol] = dest_states.copy()
         return new_nfa
-    
+
     def union(self, other: "NFA") -> "NFA":
         # the resulting NFA will accept strings that either self or other do
         new_nfa = self.copy()
-        new_nfa.states += other.states
+        new_nfa.states |= other.states
         new_nfa.accept |= other.accept
         old_starts = new_nfa.start, other.start
         start = State()
@@ -61,9 +79,15 @@ class NFA:
     def concat(self, other: "NFA") -> "NFA":
         # the resulting NFA will accept string that are accepted by self followed by other
         new_nfa = self.copy()
-        new_nfa.states += other.states
+        new_nfa.states |= other.states
         for accept in new_nfa.accept:
             new_nfa.add_transition(accept, other.start, EPSILON)
+
+        # add the transactions of other
+        for state, transitions in other.trasitions.items():
+            for symbol, dest_states in transitions.items():
+                for dest in dest_states:
+                    new_nfa.add_transition(state, dest, symbol)
         new_nfa.accept = other.accept
         return new_nfa
 
@@ -72,14 +96,14 @@ class NFA:
         new_nfa = self.copy()
         old_start = new_nfa.start
         new_nfa.start = start_and_accept
-        
+
         new_nfa.add_transition(new_nfa.start, old_start, EPSILON)
         for accept in new_nfa.accept:
             new_nfa.add_transition(accept, new_nfa.start, EPSILON)
         new_nfa.accept = {start_and_accept}
 
         return new_nfa
-    
+
     def vizualize(self):
         import matplotlib.pyplot as plt
         import networkx as nx
@@ -88,45 +112,74 @@ class NFA:
 
         for state in self.states:
             if state == self.start:
-                G.add_node(state, shape="circle", color="green", style="filled")
+                G.add_node(
+                    str(state) + "<start>",
+                    shape="circle",
+                    color="green",
+                    style="filled",
+                )
             elif state in self.accept:
-                G.add_node(state, shape="circle", color="red", style="filled")
+                G.add_node(
+                    str(state) + "<accept>", shape="circle", color="red", style="filled"
+                )
             else:
                 G.add_node(state, shape="circle", color="blue", style="filled")
 
         for src, transitions in self.trasitions.items():
             for symbol, dest_states in transitions.items():
                 for dest in dest_states:
+                    src = str(src) + "<start>" if src == self.start else src
+                    src = str(src) + "<accept>" if src in self.accept else src
+                    dest = str(dest) + "<start>" if dest == self.start else dest
+                    dest = str(dest) + "<accept>" if dest in self.accept else dest
                     symbol = symbol if symbol != EPSILON else "ε"
                     G.add_edge(src, dest, label=symbol)
 
         pos = nx.spring_layout(G)
         plt.figure()
-        node_colors = [G.nodes[n].get('color', 'blue') for n in G.nodes]
-        nx.draw(G, pos, with_labels=True, node_shape="o", node_size=3000, font_size=10, node_color=node_colors)
-        edge_labels = nx.get_edge_attributes(G, 'label')
+        node_colors = [G.nodes[n].get("color", "blue") for n in G.nodes]
+        nx.draw(
+            G,
+            pos,
+            with_labels=True,
+            node_shape="o",
+            node_size=3000,
+            font_size=10,
+            node_color=node_colors,
+        )
+        edge_labels = nx.get_edge_attributes(G, "label")
         nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
         plt.show()
+
+
 class State(object):
+    name = None
+
     def __repr__(self) -> str:
-        return f"State({id(self)})"
+        return f"State({self.name})" if self.name else f"State({id(self)})"
+
+    def mark(self, name: str):
+        self.name = name
+        return self
 
 
 def compile_regex(regex: str) -> NFA:
     # ASSUME no special chars (only regex like abc)
-    nfa = NFA([], State())
-    current = nfa.start
-    for symbol in regex:
-        new_state = State()
-        nfa.add_state(new_state)
-        nfa.add_transition(current, new_state, symbol)
-        current = new_state
-    nfa.add_accept(current)
-    ST = State()
-    OTHER = NFA([ST], ST)
-    OTHER.add_transition(ST, ST, "xxd")
-    nfa = nfa.concat(OTHER)
-    return nfa
+    def parse_concat(string: str):
+        s0 = string[0]
+        rest = string[1:]
+        start = State()
+        accept = State()
+        nfa = NFA({start, accept}, start)  # nfa that accepts only s0
+        nfa.add_transition(start, accept, s0)
+        nfa.add_accept(accept)
+        if rest:
+            ret = nfa.concat(parse_concat(rest))
+        else:
+            ret = nfa
+        return ret
+
+    return parse_concat(regex)
 
 
 if __name__ == "__main__":
@@ -134,4 +187,3 @@ if __name__ == "__main__":
     nfa.vizualize()
     assert nfa.match("abc")
     assert not nfa.match("ab")
-
